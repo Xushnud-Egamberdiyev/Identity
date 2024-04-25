ee35cfaea1c92a9e2786065534a6796a13d527f5
Xushnud Egamberdiyev <140577111+Xushnud-Egamberdiyev@users.noreply.github.com>
2024-04-25 13:34:48 +0500

    update  file

---
+++ Identity/Filter/ExceptionFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Identity.Filter
+{
+    public class ExceptionFilter : Attribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var exception = context.Exception;
+
+            context.Result = new ContentResult
+            {
+                Content = "Xatolik yuz berdi, iltimos keyinroq urinib ko'ring.",
+                ContentType = "text/plain",
+                StatusCode = 500 
+            };
+
+       
+            Console.WriteLine($"Xatolik: {exception.Message}");
+        }
+    }
+    }
+}
