0f01f1c85bc6707868c84c006e880ceffa3cbdeb
Xushnud Egamberdiyev <140577111+Xushnud-Egamberdiyev@users.noreply.github.com>
2024-04-25 13:28:16 +0500

    update file

---
+++ Identity/Filter/ResourceFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Identity.Filter
+{
+    public class ResourceFilter : Attribute, IResourceFilter
+    {
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            context.HttpContext.Response.Headers.Add("X-Custom-Header", "MyCustomValue");
+        }
+    }
+}
---
+++ Identity/Filter/ResultFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Identity.Filter
+{
+    public class ResultFilter : Attribute, IResultFilter
+    {
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            context.HttpContext.Response.Headers.Add("DateTime", DateTime.Now.ToString());
+        }
+    }
+}
